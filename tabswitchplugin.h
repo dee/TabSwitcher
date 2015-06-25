f2d8d7862f22d41db439656eb3667ebcf57b10a6
Dee <priamid@gmail.com>
2015-06-25 13:31:37 +0200

    Dev: same as previous commit.

--- tabswitchplugin.h
+++ tabswitchplugin.h
@@ -9,6 +9,11 @@ class QStringList;
 class QString;
 class QVBoxLayout;
 
+namespace Core
+{
+	class EditorManagerPlaceHolder;
+}
+
 namespace TabSwitch
 {
 	class FancyTabBar;
@@ -29,11 +34,23 @@ namespace TabSwitch
 			ShutdownFlag aboutToShutdown();
 		public slots:
 			void modeChanged(Core::IMode *mode);
+			void editorCreated(Core::IEditor *editor, const QString &fileName);
+			void currentEditorChanged(Core::IEditor *editor);
+			void editorsClosed(QList<Core::IEditor *> editors);
+			void currentChanged(int);
+			void customContextMenu(const QPoint &);
+			void closeOthers();
 		private:
 			FancyTabBar *m_tab;
 			QVBoxLayout* m_placeHolderLayout;
+			Core::EditorManagerPlaceHolder* m_placeHolder;
 			Core::ICore *m_core;
+			QMenu *m_popup;
+			QAction *m_closeOthers;
 			bool m_initialised;
+
+			QString getTabCaption(const QString&);
+			int findTab(Core::IEditor *editor);
 		};
 
 	} // namespace Internal
